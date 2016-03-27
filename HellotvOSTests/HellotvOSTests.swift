be3c83659994d922158eeaee638cd39b766a069c
Pedro Ruíz <pedro.ruiz.diaz@me.com>
2016-03-27 16:40:37 +0900

    Initial Commit

---
+++ HellotvOSTests/HellotvOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  HellotvOSTests.swift
+//  HellotvOSTests
+//
+//  Created by Pedro Ruíz on 3/27/16.
+//  Copyright © 2016 Pedro Ruíz. All rights reserved.
+//
+
+import XCTest
+@testable import HellotvOS
+
+class HellotvOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
