b0241916a98145c441a3c23689702c013d125671
Pedro Ruíz <pedro.ruiz.diaz@me.com>
2016-03-28 11:23:49 +0900

    Add streaming video logic, button identifiers and event listeners

--- HellotvOS/main.js
+++ HellotvOS/main.js
@@ -3,6 +3,10 @@ App.onLaunch = function(options) {
   var resourceLoader = new ResourceLoaderJS(NativeResourceLoader.create());
   var initialDoc = resourceLoader.getDocument("hello.tvml");
   navigationDocument.pushDocument(initialDoc);
+  
+  initialDoc.addEventListener("select",handleEvent);
+  initialDoc.addEventListener("play",hadleEvent);
+
 };
 
 class ResourceLoaderJS {
@@ -14,5 +18,24 @@ class ResourceLoaderJS {
   getDocument(name) {
     var docString = this.nativeResourceLoader.loadBundleResource(name);
     return this.domParser.parseFromString(docString,"application/xml");
+  }
+}
+
+function playVideo(title,url) {
+  var player = new Player();
+  var video = new MediaItem('video',url);
+  video.title = title;
+  
+  player.playlist = new Playlist();
+  player.playlist.push(video);
+  
+  player.play();
+}
+
+function handleEvent(event) {
+  var buttonId = event.target.getAttribute("id");
+  
+  if (buttonId === "play") {
+    playVideo("Hello TVML!", "https://s3.amazonaws.com/tvos-apprentice/helloTVML/hls_playlist.m3u8");
   }
 }
